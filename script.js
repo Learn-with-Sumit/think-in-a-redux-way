37018411e33a1c1546dc66304b6336d59882598e
Sumit Saha <sumit@learnwithsumit.com>
2022-04-21 15:24:33 +0600

    Lesson 1.7

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
+// action identifiers
+const INCREMENT = "increment";
+const DECREMENT = "increment";
+
+// action creators
+const increment = (value) => {
+    return {
+        type: INCREMENT,
+        payload: value,
+    };
+};
+
+const decrement = (value) => {
+    return {
+        type: DECREMENT,
+        payload: value,
+    };
+};
+
 // initial state
 const initialState = {
     value: 0,
@@ -10,15 +29,15 @@ const initialState = {
 
 // create reducer function
 function counterReducer(state = initialState, action) {
-    if (action.type === "increment") {
+    if (action.type === INCREMENT) {
         return {
             ...state,
-            value: state.value + 1,
+            value: state.value + action.payload,
         };
-    } else if (action.type === "decrement") {
+    } else if (action.type === DECREMENT) {
         return {
             ...state,
-            value: state.value - 1,
+            value: state.value - action.payload,
         };
     } else {
         return state;
@@ -40,13 +59,9 @@ store.subscribe(render);
 
 // button click listeners
 incrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "increment",
-    });
+    store.dispatch(increment(3));
 });
 
 decrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "decrement",
-    });
+    store.dispatch(decrement(2));
 });
