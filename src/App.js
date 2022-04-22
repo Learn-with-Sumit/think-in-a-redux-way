acd23113a86d43e314641ecb5a2f05b3770b96ee
Sumit Saha <sumit@learnwithsumit.com>
2022-04-23 00:46:59 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import Counter from "./components/Counter";
 
 export default function App() {
     return (
-        <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-            <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+        <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+            <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
                 Simple Counter Application
             </h1>
 
-            <div class="max-w-md mx-auto mt-10 space-y-5">
+            <div className="max-w-md mx-auto mt-10 space-y-5">
                 <Counter />
                 <Counter />
                 <Counter />
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,17 +12,17 @@ export default function Counter() {
     };
 
     return (
-        <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-            <div class="text-2xl font-semibold">{count}</div>
-            <div class="flex space-x-3">
+        <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+            <div className="text-2xl font-semibold">{count}</div>
+            <div className="flex space-x-3">
                 <button
-                    class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                    className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
                     onClick={increment}
                 >
                     Increment
                 </button>
                 <button
-                    class="bg-red-400 text-white px-3 py-2 rounded shadow"
+                    className="bg-red-400 text-white px-3 py-2 rounded shadow"
                     onClick={decrement}
                 >
                     Decrement
