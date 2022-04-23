8ed329b8eb7c2b65738fbc07dd8018f0c8b04c58
Sumit Saha <sumit@learnwithsumit.com>
2022-04-23 17:46:27 +0600

    Lesson 2.11

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import myLogger from "./middlewares/myLogger";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger, myLogger))
+);
 
 export default store;
