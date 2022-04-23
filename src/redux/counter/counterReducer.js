e0fd9e0406f6a5cd7e4e7909d4bbd75f65a70a91
Sumit Saha <sumit@learnwithsumit.com>
2022-04-23 13:32:38 +0600

    Lesson 2.2

---
+++ src/redux/counter/counterReducer.js
@@ -0,0 +1,26 @@
+import { DECREMENT, INCREMENET } from "./actionTypes";
+
+const initialState = {
+    value: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENET:
+            return {
+                ...state,
+                value: state.value + 1,
+            };
+
+        case DECREMENT:
+            return {
+                ...state,
+                value: state.value - 1,
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default counterReducer;
