08ef7da93d86af3a0ff71efb40f5c14301ee96f6
Sumit Saha <sumit@learnwithsumit.com>
2022-04-23 17:08:03 +0600

    Lesson 2.10

---
+++ src/redux/middlewares/myLogger.js
@@ -0,0 +1,16 @@
+import rootReducer from "../rootReducer";
+
+// create our first middleware
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const upcomingState = [action].reduce(rootReducer, store.getState());
+
+    console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
+
+    // pass action
+    return next(action);
+};
+
+export default myLogger;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import myLogger from "./middlewares/myLogger";
 import rootReducer from "./rootReducer";
 
-// create our first middleware
-const myLogger = (store) => (next) => (action) => {
-    console.log(`Action: ${JSON.stringify(action)}`);
-    console.log(`Before: ${JSON.stringify(store.getState())}`);
-
-    const upcomingState = [action].reduce(rootReducer, store.getState());
-
-    console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
-
-    // pass action
-    return next(action);
-};
-
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
 
 export default store;
