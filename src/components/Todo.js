50effca86ecf9a9458c72d76fc755e53bc9108a5
Sumit Saha <sumit@learnwithsumit.com>
2022-04-26 16:11:55 +0600

    Lesson 3.6

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,35 +1,76 @@
+import { useDispatch } from "react-redux";
 import cancelImage from "../assets/images/cancel.png";
+import { colorSelected, deleted, toggled } from "../redux/todos/actions";
 
-export default function Todo() {
+export default function Todo({ todo }) {
+    const dispatch = useDispatch();
+
+    const { text, id, completed, color } = todo;
+
+    const handleStatusChange = (todoId) => {
+        dispatch(toggled(todoId));
+    };
+
+    const handleColorChange = (todoId, color) => {
+        dispatch(colorSelected(todoId, color));
+    };
+
+    const handleDelete = (todoId) => {
+        dispatch(deleted(todoId));
+    };
+
     return (
         <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
-            <div className="rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 border-green-500 focus-within:border-green-500">
+            <div
+                className={`rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${
+                    completed &&
+                    "border-green-500 focus-within:border-green-500"
+                }`}
+            >
                 <input
                     type="checkbox"
+                    checked={completed}
+                    onChange={() => handleStatusChange(id)}
                     className="opacity-0 absolute rounded-full"
                 />
-                <svg
-                    className="hidden fill-current w-3 h-3 text-green-500 pointer-events-none"
-                    viewBox="0 0 20 20"
-                >
-                    <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
-                </svg>
+                {completed && (
+                    <svg
+                        className="fill-current w-3 h-3 text-green-500 pointer-events-none"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+                    </svg>
+                )}
             </div>
 
-            <div className="select-none flex-1 line-through">
-                Learn React from Learn with Sumit YouTube Channel
-            </div>
+            <div className="select-none flex-1 line-through">{text}</div>
 
-            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500 bg-green-500"></div>
+            <div
+                className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-green-500 border-green-500 ${
+                    color === "green" && "bg-green-500"
+                }`}
+                onClick={() => handleColorChange(id, "green")}
+            ></div>
 
-            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-yellow-500 hover:bg-yellow-500"></div>
+            <div
+                className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-yellow-500 border-yellow-500 ${
+                    color === "yellow" && "bg-yellow-500"
+                }`}
+                onClick={() => handleColorChange(id, "yellow")}
+            ></div>
 
-            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500"></div>
+            <div
+                className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-red-500 border-red-500 ${
+                    color === "red" && "bg-red-500"
+                }`}
+                onClick={() => handleColorChange(id, "red")}
+            ></div>
 
             <img
                 src={cancelImage}
                 className="flex-shrink-0 w-4 h-4 ml-2 cursor-pointer"
                 alt="Cancel"
+                onClick={() => handleDelete(id)}
             />
         </div>
     );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
 import Todo from "./Todo";
 
 export default function TodoList() {
+    const todos = useSelector((state) => state.todos);
+
     return (
         <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-            <Todo />
+            {todos.map((todo) => (
+                <Todo todo={todo} key={todo.id} />
+            ))}
         </div>
     );
 }
