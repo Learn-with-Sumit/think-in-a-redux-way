cef12a3e2ee9cfac065032695cf1d3f01b80ee23
Sumit Saha <sumit@learnwithsumit.com>
2022-04-26 15:07:32 +0600

    Lesson 3.3

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+export const initialState = [
+    {
+        id: 1,
+        text: "Learn React JS",
+        completed: true,
+    },
+    {
+        id: 2,
+        text: "Learn Redux",
+        completed: false,
+        color: "red",
+    },
+];
---
+++ src/redux/todos/reducer.js
@@ -0,0 +1,69 @@
+import {
+    ADDED,
+    ALLCOMPLETED,
+    CLEARCOMPLETED,
+    COLORSELECTED,
+    DELETED,
+    TOGGLED,
+} from "./actionTypes";
+import initialState from "./initialState";
+
+const nextTodoId = (todos) => {
+    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+    return maxId + 1;
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADDED:
+            return [
+                ...state,
+                {
+                    id: nextTodoId(state),
+                },
+            ];
+
+        case TOGGLED:
+            return state.map((todo) => {
+                if (todo.id !== action.payload) {
+                    return todo;
+                }
+
+                return {
+                    ...todo,
+                    completed: !todo.completed,
+                };
+            });
+
+        case COLORSELECTED:
+            const { todoId, color } = action.payload;
+            return state.map((todo) => {
+                if (todo.id !== todoId) {
+                    return todo;
+                }
+                return {
+                    ...todo,
+                    color: color,
+                };
+            });
+
+        case DELETED:
+            return state.filter((todo) => todo.id !== action.payload);
+
+        case ALLCOMPLETED:
+            return state.map((todo) => {
+                return {
+                    ...todo,
+                    completed: true,
+                };
+            });
+
+        case CLEARCOMPLETED:
+            return state.filter((todo) => !todo.completed);
+
+        default:
+            break;
+    }
+};
+
+export default reducer;
