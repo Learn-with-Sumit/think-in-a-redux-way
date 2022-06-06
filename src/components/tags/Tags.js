d10192cb004608de886828d0873dbf406467473b
Sumit Saha <sumit@learnwithsumit.com>
2022-06-06 14:13:09 +0600

    changes

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -3,7 +3,7 @@ import Tag from "./Tag";
 export default function Tags() {
     return (
         <section>
-            <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-scroll">
+            <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
                 <Tag />
             </div>
         </section>
