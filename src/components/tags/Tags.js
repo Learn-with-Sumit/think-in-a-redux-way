9101daab5e7641150a6185727d59abab0fd4e764
Sumit Saha <sumit@learnwithsumit.com>
2022-06-06 14:10:27 +0600

    changes

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -13,7 +13,7 @@ export default function Tags() {
 
     return tags?.length > 0 ? (
         <section>
-            <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-scroll">
+            <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
                 {tags.map((tag) => (
                     <Tag key={tag.id} title={tag.title} />
                 ))}
