69226e1273285ca7d54c780cf0b17a984b5d5ccd
Sumit Saha <sumit@learnwithsumit.com>
2022-06-16 13:43:53 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import Balance from "./components/Balance";
-import Form from "./components/Form";
-import Layout from "./components/Layout";
-import Transactions from "./components/Transactions/Transactions";
-
 function App() {
     return (
-        <Layout>
-            <Balance />
-            <Form />
-            <Transactions />
-        </Layout>
+        <div className="App">
+            <div>Hello world</div>
+        </div>
     );
 }
 
