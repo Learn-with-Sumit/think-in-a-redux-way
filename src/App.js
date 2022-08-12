4bfe6effe13bfee7a7f038c90b7467a21367ff4f
Shamim Al-Mamun <50411936+samimalmamuncse@users.noreply.github.com>
2022-08-12 19:44:50 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
                 <Counter />
                 <Counter />
                 <Counter />
+                <Counter />
             </div>
         </div>
     );
