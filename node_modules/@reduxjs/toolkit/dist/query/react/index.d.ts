035b5e52ed9370fd4d63a35906e1ad612fc40c1a
Sumit Saha <sumit@learnwithsumit.com>
2022-08-20 20:14:31 +0600

    changes

--- node_modules/@reduxjs/toolkit/dist/query/react/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { CreateApi } from '@reduxjs/toolkit/query';
-import { reactHooksModule, reactHooksModuleName } from './module';
-export * from '@reduxjs/toolkit/query';
-export { ApiProvider } from './ApiProvider';
-declare const createApi: CreateApi<typeof import("@reduxjs/toolkit/dist/query/core/module").coreModuleName | typeof reactHooksModuleName>;
-export { createApi, reactHooksModule };
