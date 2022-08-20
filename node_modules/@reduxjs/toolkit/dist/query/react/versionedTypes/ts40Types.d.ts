9542076fbd9cf11b45da7d5f3395879e747a5397
Sumit Saha <sumit@learnwithsumit.com>
2022-08-20 20:12:01 +0600

    changes

--- node_modules/@reduxjs/toolkit/dist/query/react/versionedTypes/ts40Types.d.ts
+++
@@ -1,3 +0,0 @@
-import type { EndpointDefinitions } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
-export declare type HooksWithUniqueNames<Definitions extends EndpointDefinitions> = unknown;
-export {};
