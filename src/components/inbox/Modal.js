b5539d1f3c9221279b8a6860167accc004462286
Sumit Saha <sumit@learnwithsumit.com>
2022-09-07 17:15:33 +0600

    changes

--- src/components/inbox/Modal.js
+++ src/components/inbox/Modal.js
@@ -1,4 +1,29 @@
+import { useState } from "react";
+import isValidEmail from "../../utils/isValidEmail";
+
 export default function Modal({ open, control }) {
+    const [to, setTo] = useState("");
+    const [message, setMessage] = useState("");
+
+    const debounceHandler = (fn, delay) => {
+        let timeoutId;
+        return (...args) => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                fn(...args);
+            }, delay);
+        };
+    };
+
+    const doSearch = (value) => {
+        if (isValidEmail(value)) {
+            // check user API
+            setTo(value);
+        }
+    };
+
+    const handleSearch = debounceHandler(doSearch, 500);
+
     return (
         open && (
             <>
@@ -10,8 +35,7 @@ export default function Modal({ open, control }) {
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                         Send message
                     </h2>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
-                        <input type="hidden" name="remember" value="true" />
+                    <form className="mt-8 space-y-6">
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label htmlFor="to" className="sr-only">
@@ -20,10 +44,13 @@ export default function Modal({ open, control }) {
                                 <input
                                     id="to"
                                     name="to"
-                                    type="to"
+                                    type="email"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Send to"
+                                    onChange={(e) =>
+                                        handleSearch(e.target.value)
+                                    }
                                 />
                             </div>
                             <div>
@@ -33,10 +60,12 @@ export default function Modal({ open, control }) {
                                 <textarea
                                     id="message"
                                     name="message"
-                                    type="message"
+                                    type="text"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Message"
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.value)}
                                 />
                             </div>
                         </div>
