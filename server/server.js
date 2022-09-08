f318dcb30f2ac1bcd0e833759f690dd167289aeb
Sumit Saha <sumit@learnwithsumit.com>
2022-09-08 16:58:29 +0600

    changes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const auth = require("json-server-auth");
 const jsonServer = require("json-server");
+const express = require("express");
+const http = require("http");
 
-const server = jsonServer.create();
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+global.io = io;
+
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
 // Bind the router db to the app
-server.db = router.db;
+app.db = router.db;
 
-server.use(middlewares);
+app.use(middlewares);
 
 const rules = auth.rewriter({
     users: 640,
@@ -17,8 +24,8 @@ const rules = auth.rewriter({
     messages: 660,
 });
 
-server.use(rules);
-server.use(auth);
-server.use(router);
+app.use(rules);
+app.use(auth);
+app.use(router);
 
 server.listen(port);
