ddd436b00310785715ee3462bd3d90323ab1165a
Sumit Saha <sumit@learnwithsumit.com>
2022-09-09 18:55:31 +0600

    changes

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,18 +1,29 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { userLoggedOut } from "../auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_API_URL,
+    prepareHeaders: async (headers, { getState, endpoint }) => {
+        const token = getState()?.auth?.accessToken;
+        if (token) {
+            headers.set("Authorization", `Bearer ${token}`);
+        }
+
+        return headers;
+    },
+});
 
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: process.env.REACT_APP_API_URL,
-        prepareHeaders: async (headers, { getState, endpoint }) => {
-            const token = getState()?.auth?.accessToken;
-            if (token) {
-                headers.set("Authorization", `Bearer ${token}`);
-            }
+    baseQuery: async (args, api, extraOptions) => {
+        let result = await baseQuery(args, api, extraOptions);
 
-            return headers;
-        },
-    }),
+        if (result?.error?.status === 401) {
+            api.dispatch(userLoggedOut());
+            localStorage.clear();
+        }
+        return result;
+    },
     tagTypes: [],
     endpoints: (builder) => ({}),
 });
