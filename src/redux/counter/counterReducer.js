34029e72b32b0bc72d8431ed8ed047ca0475187e
sparkles: Saad Hasan <saadh393@gmail.com>
2023-02-19 16:15:14 +0600

    :bug: Dynamic Dispatch Issue Fixed

--- src/redux/counter/counterReducer.js
+++ src/redux/counter/counterReducer.js
@@ -1,26 +1,26 @@
 import { DECREMENT, INCREMENET } from "./actionTypes";
 
 const initialState = {
-    value: 20,
+  value: 20,
 };
 
 const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case INCREMENET:
-            return {
-                ...state,
-                value: state.value + 1,
-            };
+  switch (action.type) {
+    case INCREMENET:
+      return {
+        ...state,
+        value: state.value + action.payload,
+      };
 
-        case DECREMENT:
-            return {
-                ...state,
-                value: state.value - 1,
-            };
+    case DECREMENT:
+      return {
+        ...state,
+        value: state.value - action.payload,
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default counterReducer;
